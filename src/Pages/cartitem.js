3593e7fb8e4e652cbea3e0547eddaf7e0e62cc65
ianotieno <ianotieno23@gmail.com>
2023-08-02 00:05:19 +0300

    update on cart component

--- src/Pages/cartitem.js
+++ src/Pages/cartitem.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import "./cart.css"
+import {ShopContext} from '../context/shop-context';
 const CartItem = (props) => {
     const { id, productName, price, productImage } = props.data;
+    const {cartItems, addToCart , removeFromCart}=useContext(ShopContext);
   return (
     <div className="cartItem">
       <img src={productImage}/>
       <div className="description"></div>
       <p><b>{productName}</b></p>
       <p><b>ksh: {price}</b></p>
+      <div className="countHandler"> 
+      <button onClick={()=>removeFromCart(id)}>-</button>
+      <input value={cartItems[id]}/>
+      <button onClick={()=>addToCart(id)}>+</button>
+      </div>
     </div>
+
   )
 }
 
